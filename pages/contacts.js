5f174bf7326cec48c75f60fed3cdbec1ef2da43e
Greg McNally <gmcnally@bearcubs.santarosa.edu>
2021-12-15 10:26:20 -0800

    Update contacts.js
    deleted import getAbsoluteURL from '../utils/getAbsoluteURL' file

--- pages/contacts.js
+++ pages/contacts.js
@@ -37,7 +37,6 @@ import {
     withAuthUserTokenSSR,
     AuthAction,
 } from 'next-firebase-auth'
-import getAbsoluteURL from '../utils/getAbsoluteURL'
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import Header from '../components/Header'
